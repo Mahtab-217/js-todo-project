0ebadd320bb2a9abdeebef8ca4663088f9a71562
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-24 07:21:03 +0430

    new and updated code

--- todo.js
+++ todo.js
@@ -1,2 +1,18 @@
 const parent=document.getElementById("container");
+const addButton=document.getElementById("addBtn");
+const containerModal=document.getElementById("container-modal");
 const todoList=[];
+addButton.addEventListener("click",()=>{
+    if(!containerModal.classList.contains("toggleForm")){
+      containerModal.classList.add("toggleForm");
+    addButton.textContent("Hide Form")
+    }
+    else{
+        containerModal.classList.remove("toggleForm");
+        addButton.classList.add("Add Todo")
+    }
+})
+{
+    id: 1;
+    name:"Reading a book";
+}
