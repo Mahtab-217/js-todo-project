945f5c381e06bb79b250bfb5e42441b3f8d0c386
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-27 21:16:38 +0430

    last part of project

--- todo.js
+++ todo.js
@@ -25,6 +25,7 @@ function saveTodo(){
    todoList.push({
     todo,
     importance,
+    compelted: false,
    });
    document.getElementById("todo").value=""
    document.getElementById("importance").value=""
@@ -36,9 +37,16 @@ function saveTodo(){
 // console.log(todo, importance)
 }
 function showTodo(){
-for(let todo of todoList){
+  const sortedList=  todoList.sort((a,b)=>a.compelted-b.compelted);
+for(let todo of sortedList){
  
  const div= document.createElement("div");
+ div.addEventListener("click",()=>{
+    console.log("clicked");
+    todo.compelted = !todo.compelted;
+    parent.innerHTML="";
+    showTodo();
+ })
  div.classList.add("box")
  const h1 = document.createElement("h1");
  h1.textContent= todo.todo;
@@ -53,6 +61,9 @@ for(let todo of todoList){
  }
  else{
     smallBox.classList.add("green");
+ }
+ if(todo.compelted){
+    h1.style.textDecoration ="line-through";
  }
     div.append(h1, smallBox);
     parent.append(div);
